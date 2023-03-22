5c0a14ccad2f712a57611374e154d423a6b49454
Nicolas Ibba Gonzalez <nicolasig002@gmail.com>
2023-03-21 21:37:30 -0300

    Day one!

---
+++ Day1/excersise.py
@@ -0,0 +1,57 @@
+# https://github.com/Asabeneh/30-Days-Of-Python#-exercises---day-1
+# Level 2
+import sys
+import math
+print(sys.version)
+#============================= 2 =============================
+print(3 + 4) #Addition
+print(3 - 4) #Substraction
+print(3 * 4) #Multiplication
+print(3 % 4) #Modulus
+print(3 / 4) #Division
+print(3 ** 4) #Exponential
+print(3 // 4) #Floor division
+
+#============================= 3 =============================
+print("Axyz")
+print("Nicolas Ibba Gonzalez | My name")
+print("Argentina")
+print("I'm enjoying 30 days of python")
+
+#============================= 4 =============================
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Axyz02','Python','Argentina']))
+print(type('Axyz'))
+print(type('Argentina'))
+print(type('Python'))
+
+# Level 3
+
+#Number
+#Integer
+1
+#Float
+1.1
+#Complex
+3J
+#Boolean
+True
+#List
+['Nicolas','Agustina','Ambar','Iris','Hestia']
+#Tuple
+('Nicolas','Agustina','Ambar','Iris','Hestia')
+#Dictionary
+{
+    'name':'Nicolas',
+    'last_name':'Ibba Gonzalez',
+    'country': 'Argentina',
+    'age': 24
+}
+#Set
+{2,5,3,1,4}
+
+#2
+print(math.dist([2,3],[10,8]))
